9e016157c7601b672b64765d13221738df8222a1
Anees PK <anees.pk@appfabs.com>
2019-11-20 13:03:43 +0530

    Update index.js api call

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  console.log('Time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  var request = require("request");
+  var api = "https://api.beaglesecurity.com/v1/test/start";
+  var requestData = {
+    "access_token": "ikfoh0fpdluw0rvglrtonipe46ctu7bc",
+    "application_token": "8y81s7e3d88zqp4lzdllq0nacfao3i21"
+};
+  request({
+      url: api,
+      method: "POST",
+      json: requestData
+  }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+          console.log(body);
+      }
+      else {
+          console.log("response.statusCode: " + response.statusCode);
+          console.log("response.statusText: " + response.statusText);
+      }
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
